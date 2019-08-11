7521bc211edb2989944bee54f389e521267f947d
Carl-Stanley <cds.beml@gmail.com>
2019-08-11 08:58:02 -0700

    Commit

--- client/src/App.js
+++ client/src/App.js
@@ -17,24 +17,18 @@ export default class App extends Component {
         this.setState({ savedList });
     }
     render() {
-        return ( <
-            div >
-            <
-            SavedList list = { this.state.savedList }
-            /> <
-            Route exact path = "/"
-            component = { MovieList }
-            /> <
-            Route path = "/movies/:id"
-            render = {
+        return ( <div>
+            <SavedList list = { this.state.savedList }/> 
+            <Route exact path = "/" component = { MovieList }/> 
+            <Route path = "/movies/:id"
+             render = {
                 (props) => {
                     return ( < Movie {...props }
                         addToSavedList = { this.addToSavedList }
                         />)
                     }
                 }
-                /> <
-                /div>
+                /> </div>
             )
         }
     }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -18,40 +18,34 @@ const MovieList = props => {
         getMovies();
     }, []);
 
-    return ( <
-        div className = "movie-list" > {
+    return ( <div className = "movie-list"> {
             movies.map(movie => ( <
                 MovieDetails key = { movie.id }
                 movie = { movie }
                 />
             ))
-        } <
-        /div>
+        } </div>
     );
 }
 
 function MovieDetails({ movie }) {
     const { title, director, metascore, stars } = movie;
-    return ( <
-        div className = "movie-card" >
-        <
-        h2 > { title } < /h2> <
-        div className = "movie-director" >
-        Director: < em > { director } < /em> < /
-        div > <
-        div className = "movie-metascore" >
-        Metascore: < strong > { metascore } < /strong> < /
-        div > <
-        h3 > Actors < /h3>
+    return ( <div className = "movie-card" >
+        <h2> { title } </h2> 
+        <div className = "movie-director" >
+        Director: <em > { director } </em> 
+        </div> 
+        <div className = "movie-metascore" >
+        Metascore: <strong > { metascore } </strong> 
+        </div>
+         <h3> Actors </h3>
 
         {
-            stars.map(star => ( <
-                div key = { star }
-                className = "movie-star" > { star } <
-                /div>
+            stars.map(star => ( <div key = { star }
+                className = "movie-star"> { star } 
+                </div>
             ))
-        } <
-        /div>
+        } </div>
     );
 }
 
